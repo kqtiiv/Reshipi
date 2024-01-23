63b58e40652a0d6f09941ce2158384a03b2b384a
Katie Wan <katiewan.kw@gmail.com>
2024-01-23 23:27:26 +0000

    Remove api-key

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,23 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 
-const options = {
-    method: 'POST',
-    url: 'https://zestful.p.rapidapi.com/parseIngredients',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '6fda833675mshfaca7ba105d3a3cp13d275jsnd9f1899318c3',
-      'X-RapidAPI-Host': 'zestful.p.rapidapi.com'
-    },
-    data: {
-      ingredients: [
-        '2 1/2 tablespoons finely chopped parsley',
-        '3 tablespoons extra-virgin olive oil, or more to taste',
-        '1 1/2 tbsp cinnamon'
-      ]
-    }
-  };
-
 router.get("/", (req, res) => {
     const data = {
         title: "Home",
