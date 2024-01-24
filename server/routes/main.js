8fd8242c554faa748a5aa520589244909e25cbea
Katie Wan <katiewan.kw@gmail.com>
2024-01-24 22:34:51 +0000

    Add new webpages

--- server/routes/main.js
+++ server/routes/main.js
@@ -9,4 +9,25 @@ router.get("/", (req, res) => {
     res.render("index", data);
 });
 
+router.get("/explore", (req, res) => {
+  const data = {
+    title: "Explore",
+  }
+  res.render("explore", data);
+});
+
+router.get("/about", (req, res) => {
+  const data = {
+    title: "About",
+  }
+  res.render("about", data);
+});
+
+router.get("/contact", (req, res) => {
+  const data = {
+    title: "Contact",
+  }
+  res.render("contact", data);
+});
+
 module.exports = router;
